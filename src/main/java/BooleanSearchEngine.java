3957fb6858487a093131f5aa0c96a5f72ec3927c
Tamara Tatarintseva <tta.nemo@gmail.com>
2023-05-22 00:25:43 +0300

    initial commit

--- src/main/java/BooleanSearchEngine.java
+++ src/main/java/BooleanSearchEngine.java
@@ -1,18 +1,66 @@
+import com.itextpdf.kernel.pdf.PdfDocument;
+import com.itextpdf.kernel.pdf.PdfReader;
+import com.itextpdf.kernel.pdf.canvas.parser.PdfTextExtractor;
+
 import java.io.File;
 import java.io.IOException;
 import java.util.*;
 
+/**
+ * Реализация поискового движка.
+ * Слово Boolean пришло из теории информационного поиска, тк наш движок будет искать в тексте ровно то слово,
+ * которое было указано, без использования синонимов и прочих приёмов нечёткого поиска
+ */
+
 public class BooleanSearchEngine implements SearchEngine {
-    //???
+    private final Map<String, List<PageEntry>> map = new HashMap<>();
 
     public BooleanSearchEngine(File pdfsDir) throws IOException {
-        // прочтите тут все pdf и сохраните нужные данные,
-        // тк во время поиска сервер не должен уже читать файлы
+        for (File pdf : pdfsDir.listFiles()) {
+            var document = new PdfDocument(new PdfReader(pdf));
+            int pageNumber = document.getNumberOfPages();
+            for (int i = 1; i <= pageNumber; i++) {
+                var page = document.getPage(i);
+                var text = PdfTextExtractor.getTextFromPage(page);
+                var words = text.split("\\P{IsAlphabetic}+");
+
+                Map<String, Integer> freqs = new HashMap<>(); // мапа, где ключом будет слово, а значением - частота
+                for (var word : words) { // перебираем слова
+                    if (word.isEmpty()) {
+                        continue;
+                    }
+                    word = word.toLowerCase();
+                    freqs.put(word, freqs.getOrDefault(word, 0) + 1);
+                }
+
+                for (String word : freqs.keySet()) {
+                    PageEntry pageEntry = new PageEntry(pdf.getName(), i, freqs.get(word));
+                    if (map.containsKey(word)) {
+                        map.get(word).add(pageEntry);
+
+                    } else {
+                        map.put(word, new ArrayList<>());
+                        map.get(word).add(pageEntry);
+                    }
+                    //Сортировка
+                    map.values().forEach(Collections::sort);
+                }
+            }
+        }
     }
 
     @Override
     public List<PageEntry> search(String word) {
-        // тут реализуйте поиск по слову
-        return Collections.emptyList();
+        //Создаем пустой список с результатами поиска
+        List<PageEntry> result = new ArrayList<>();
+        //Переводим слова в нижний регистра
+        String wordToLowerCase = word.toLowerCase();
+        //Если слово есть в базе - перебираем результаты поиска и добавляем их в список
+        if (map.get(wordToLowerCase) != null) {
+            for (PageEntry pageEntry : map.get(wordToLowerCase)) {
+                result.add(pageEntry);
+            }
+        }
+        return result;
     }
 }
--- src/main/java/PageEntry.java
+++ src/main/java/PageEntry.java
@@ -1,7 +1,37 @@
+import org.json.JSONObject;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+/**
+ * Класс, описывающий один элемент результата одного поиска.
+ * Он состоит из имени пдф-файла, номера страницы и количества раз, которое встретилось это слово на ней.
+ */
+
 public class PageEntry implements Comparable<PageEntry> {
     private final String pdfName;
     private final int page;
     private final int count;
 
-    // ???
+    public PageEntry(String pdfName, int page, int count) {
+        this.pdfName = pdfName;
+        this.page = page;
+        this.count = count;
+    }
+
+    @Override
+    public int compareTo(PageEntry o) {
+        return o.count - this.count;
+    }
+
+    @Override
+    public String toString() {
+        Map resultMap = new LinkedHashMap();
+        resultMap.put("pdfName", pdfName);
+        resultMap.put("page", page);
+        resultMap.put("count", count);
+        JSONObject result = new JSONObject(resultMap);
+        return result.toString();
+    }
 }
+
